e1010c7521e2ba705f355c9f128e60251c9c4ee2
Brady <brady.johnston@me.com>
2021-10-01 15:16:18 +0800

    added b_plot_coefs()

--- R/plot.R
+++ R/plot.R
@@ -54,3 +54,68 @@ b_plot <- function(data, ..., facet = TRUE) {
 
   plt
 }
+
+#' Comparison Plot of Estimated Terms
+#'
+#' @details Creates a dot-and-whisker plot for quick comparison of estimated
+#'   terms, also showing the calculated standard errors as errorbars.
+#'
+#' @param data Nested [`tibble`] that is the result from one of the fitting functions.
+#' @param term Term to be plotted as string, i.e. `"kd"` or `"rate"`.
+#' @param ... Additional arguments to be passed to `ggplot2::aes()` such as
+#'   `colour = sample`.
+#' @param pointSize Size of points, defaults to `3`.
+#' @param group Column that results are grouped by.
+#' @param errorBarHeight Relative height of the errorbars, defaults to `0.3`.
+#'
+#' @importFrom rlang .data
+#' @return [`ggplot`] object
+#' @export
+#'
+#' @examples
+#'
+#' DNase %>%
+#' b_binding(conc, density, Run) %>%
+#'   b_plot_coefs(Run, term = "kd", colour = Run)
+b_plot_coefs <-
+  function(data,
+           group,
+           term,
+           ...,
+           pointSize = 3,
+           errorBarHeight = 0.3) {
+
+    data <- data %>%
+      b_coefs()
+
+    data <- data[data$term %in% term,]
+
+    data %>%
+      ggplot2::ggplot(
+        mapping = ggplot2::aes(
+          x = .data$estimate,
+          y = {{ group }},
+          ...
+        )
+      ) +
+      ggplot2::geom_errorbarh(
+        ggplot2::aes(
+          xmin = .data$estimate - .data$std.error,
+          xmax = .data$estimate + .data$std.error
+        ),
+        colour = "black",
+        height = errorBarHeight
+      ) +
+      ggplot2::geom_point(size = pointSize) +
+      ggplot2::scale_colour_discrete() +
+      ggplot2::theme_light() +
+      ggplot2::theme(
+        strip.background = ggplot2::element_rect(fill = "white", colour = "gray50"),
+        strip.text = ggplot2::element_text(colour = "gray10")
+      ) -> plt
+
+    if (length(term) > 1) plt <-  plt + ggplot2::facet_wrap(~.data$term)
+
+    plt
+
+  }
